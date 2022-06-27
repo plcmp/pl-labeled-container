b56e994ea4b6f2a5fea01159442f0f95c3f02a11
Azat Yamalov <yamalov.azat@live.com>
2022-06-27 10:04:13 +0300

    new-input

--- pl-labeled-container.js
+++ pl-labeled-container.js
@@ -3,23 +3,20 @@ import { PlElement, html, css } from "polylib";
 class PlLabeledContainer extends PlElement {
     static get properties() {
         return {
-            disabled: { type: Boolean, reflectToAttribute: true },
             label: { type: String },
-            variant: { type: String, reflectToAttribute: true, value: 'vertical' }
+            variant: { type: String, reflectToAttribute: true },
+            orientation: { type: String, reflectToAttribute: true }
         };
     }
 
     static get css() {
         return css`
 			:host {
-                display: flex;
-                flex-direction: column;
+                display: inline-block;
                 box-sizing: border-box;
-				direction: var(--direction);
-				align-items: flex-start;
 				outline: none;
                 min-height: var(--base-size-md);
-                flex-shrink: 0;
+                width: inherit;
 			}
 
             label {
@@ -39,44 +36,33 @@ class PlLabeledContainer extends PlElement {
                 width: 100%;
             }
 
-			:host([variant=horizontal]) {
-                flex-direction: row;
+            :host([variant=horizontal]), :host([orientation=horizontal]) {
+                margin-left: calc(var(--has-padding) * var(--label-width, 240px));
             }
 
-            :host([variant=horizontal]) > ::slotted(*) {
-                min-height: var(--base-size-md);
-                display: flex;
-                justify-content: center;
-                align-items: center;
-            }
-
-			:host([variant=horizontal]) label{
+			:host([variant=horizontal]) label, :host([orientation=horizontal]) label{
                 width: var(--label-width, 240px);
                 min-height: var(--base-size-md);
+                margin-left: calc(0px - var(--label-width, 240px));
+                position: absolute;
             }
 
             :host([variant=horizontal]) label span{ 
                 align-self: center;
                 display: -webkit-box;
                 display: -moz-box;
-                display: -ms-flexbox;
-                -webkit-box-orient: vertical;
+≈                -webkit-box-orient: vertical;
                 -webkit-line-clamp: 2;
                 overflow: hidden;
                 text-overflow: ellipsis;
             }
 
-            :host([variant=vertical]) {
-                gap: var(--space-xs);
+            :host([variant=vertical]) label{
+                padding-bottom: var(--space-xs);
             }
-
             :host([variant=vertical]) label span{
 				width: var(--content-width, 240px);
                 overflow: hidden;
-            }
-
-            :host([variant=vertical]) > ::slotted(*) {
-                align-self: flex-start !important;
             }
     	`;
     }
@@ -90,6 +76,14 @@ class PlLabeledContainer extends PlElement {
             </label>
             <slot></slot>
 		`;
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+
+        if (!this.label) {
+            this.style.setProperty('--has-padding', 0);
+        }
     }
 }
 
