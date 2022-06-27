a6e28f5d172ae7361841766bac2158f5b0b3347a
Azat Yamalov <yamalov.azat@live.com>
2022-06-27 18:51:31 +0300

    labeled-container refactor

--- pl-labeled-container.js
+++ pl-labeled-container.js
@@ -3,9 +3,10 @@ import { PlElement, html, css } from "polylib";
 class PlLabeledContainer extends PlElement {
     static get properties() {
         return {
-            disabled: { type: Boolean, reflectToAttribute: true },
             label: { type: String },
-            variant: { type: String, reflectToAttribute: true, value: 'vertical' }
+            orientation: { type: String, reflectToAttribute: true, value: 'vertical' },
+            contentWidth: { type: Number, observer: '_contentWidthObserver' },
+            labelWidth: { type: Number, observer: '_labelWidthObserver' },
         };
     }
 
@@ -20,10 +21,16 @@ class PlLabeledContainer extends PlElement {
 				outline: none;
                 min-height: var(--base-size-md);
                 flex-shrink: 0;
+                width: var(--content-width);
+                row-gap: var(--space-xs);
 			}
 
+            :host([orientation=horizontal]) {
+                flex-direction: row;
+                width: calc(var(--label-width) + var(--content-width));
+            }
+
             label {
-                box-sizing: border-box;
                 display: flex;
                 user-select: none;
                 font: var(--header-font);
@@ -31,65 +38,73 @@ class PlLabeledContainer extends PlElement {
                 flex-shrink: 0;
             }
 
-            label[hidden] {
+            label[hidden]{
                 display: none;
             }
 
             label span {
                 width: 100%;
+                overflow: hidden;
             }
 
-			:host([variant=horizontal]) {
-                flex-direction: row;
-            }
-
-            :host([variant=horizontal]) > ::slotted(*) {
-                min-height: var(--base-size-md);
-                display: flex;
-                justify-content: center;
-                align-items: center;
-            }
-
-			:host([variant=horizontal]) label{
+			:host([orientation=horizontal]) label{
                 width: var(--label-width, 240px);
                 min-height: var(--base-size-md);
             }
 
-            :host([variant=horizontal]) label span{ 
+            :host([orientation=horizontal]) label span{ 
                 align-self: center;
                 display: -webkit-box;
                 display: -moz-box;
                 display: -ms-flexbox;
                 -webkit-box-orient: vertical;
                 -webkit-line-clamp: 2;
-                overflow: hidden;
                 text-overflow: ellipsis;
             }
 
-            :host([variant=vertical]) {
-                gap: var(--space-xs);
-            }
-
-            :host([variant=vertical]) label span{
+            :host([orientation=vertical]) label span{
 				width: var(--content-width, 240px);
-                overflow: hidden;
-            }
-
-            :host([variant=vertical]) > ::slotted(*) {
-                align-self: flex-start !important;
             }
     	`;
     }
 
     static get template() {
         return html`
-            <label title$="[[label]]" hidden$="[[!label]]">
+            <label title$="[[label]]" hidden="[[!label]]">
                 <slot name="label-prefix"></slot>
                 <span>[[label]]</span>
                 <slot name="label-suffix"></slot>
             </label>
             <slot></slot>
 		`;
+    }
+
+    connectedCallback() {
+        super.connectedCallback()
+
+        if (!this.label) {
+            this.style.setProperty('--label-width', '0px');
+        }
+
+        if (this.contentWidth) {
+            this.style.setProperty('--content-width', this.contentWidth + 'px');
+        }
+
+        if (this.labelWidth) {
+            this.style.setProperty('--label-width', this.labelWidth + 'px');
+        }
+    }
+
+    _contentWidthObserver() {
+        if (this.contentWidth) {
+            this.style.setProperty('--content-width', this.contentWidth + 'px');
+        }
+    }
+
+    _labelWidthObserver() {
+        if (this.labelWidth) {
+            this.style.setProperty('--label-width', this.labelWidth + 'px');
+        }
     }
 }
 
