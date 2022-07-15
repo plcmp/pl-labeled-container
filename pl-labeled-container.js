4cf660cb8478807eac14414e50532bf7124f3872
Azat Yamalov <yamalov.azat@live.com>
2022-07-15 18:51:09 +0300

    removed getters on props, templates and css

--- pl-labeled-container.js
+++ pl-labeled-container.js
@@ -1,83 +1,77 @@
 import { PlElement, html, css } from "polylib";
 
 class PlLabeledContainer extends PlElement {
-    static get properties() {
-        return {
-            label: { type: String },
-            orientation: { type: String, reflectToAttribute: true, value: 'vertical' },
-            contentWidth: { type: Number, observer: '_contentWidthObserver' },
-            labelWidth: { type: Number, observer: '_labelWidthObserver' },
-        };
-    }
+    static propertie = {
+        label: { type: String },
+        orientation: { type: String, reflectToAttribute: true, value: 'vertical' },
+        contentWidth: { type: Number, observer: '_contentWidthObserver' },
+        labelWidth: { type: Number, observer: '_labelWidthObserver' },
+    };
 
-    static get css() {
-        return css`
-			:host {
-                display: flex;
-                flex-direction: column;
-                box-sizing: border-box;
-				direction: var(--direction);
-				align-items: flex-start;
-				outline: none;
-                min-height: var(--base-size-md);
-                flex-shrink: 0;
-                width: auto;
-                row-gap: var(--space-xs);
-			}
+    static css = css`
+        :host {
+            display: flex;
+            flex-direction: column;
+            box-sizing: border-box;
+            direction: var(--direction);
+            align-items: flex-start;
+            outline: none;
+            min-height: var(--base-size-md);
+            flex-shrink: 0;
+            width: auto;
+            row-gap: var(--space-xs);
+        }
 
-            :host([orientation=horizontal]) {
-                flex-direction: row;
-                width: calc(var(--label-width) + var(--content-width));
-            }
+        :host([orientation=horizontal]) {
+            flex-direction: row;
+            width: calc(var(--label-width) + var(--content-width));
+        }
 
-            label {
-                display: flex;
-                user-select: none;
-                font: var(--header-font);
-                color: var(--header-color);
-                flex-shrink: 0;
-            }
+        label {
+            display: flex;
+            user-select: none;
+            font: var(--header-font);
+            color: var(--header-color);
+            flex-shrink: 0;
+        }
 
-            label[hidden]{
-                display: none;
-            }
+        label[hidden]{
+            display: none;
+        }
 
-            label span {
-                width: 100%;
-                overflow: hidden;
-            }
+        label span {
+            width: 100%;
+            overflow: hidden;
+        }
 
-			:host([orientation=horizontal]) label{
-                width: var(--label-width, 240px);
-                min-height: var(--base-size-md);
-            }
+        :host([orientation=horizontal]) label{
+            width: var(--label-width, 240px);
+            min-height: var(--base-size-md);
+        }
 
-            :host([orientation=horizontal]) label span{ 
-                align-self: center;
-                display: -webkit-box;
-                display: -moz-box;
-                display: -ms-flexbox;
-                -webkit-box-orient: vertical;
-                -webkit-line-clamp: 2;
-                text-overflow: ellipsis;
-            }
+        :host([orientation=horizontal]) label span{ 
+            align-self: center;
+            display: -webkit-box;
+            display: -moz-box;
+            display: -ms-flexbox;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: 2;
+            text-overflow: ellipsis;
+        }
 
-            :host([orientation=vertical]) label span{
-				width: var(--content-width, 240px);
-            }
-    	`;
-    }
+        :host([orientation=vertical]) label span{
+            width: var(--content-width, 240px);
+        }
+    `;
 
-    static get template() {
-        return html`
-            <label title$="[[label]]" hidden="[[!label]]">
-                <slot name="label-prefix"></slot>
-                <span>[[label]]</span>
-                <slot name="label-suffix"></slot>
-            </label>
-            <slot></slot>
-		`;
-    }
+    static template = html`
+        <label title$="[[label]]" hidden="[[!label]]">
+            <slot name="label-prefix"></slot>
+            <span>[[label]]</span>
+            <slot name="label-suffix"></slot>
+        </label>
+        <slot></slot>
+    `;
 
     connectedCallback() {
         super.connectedCallback()
